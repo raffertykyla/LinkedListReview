aebd39060574a850093649e531d52634473220bb
Kyla Rafferty <k.rafferty@baypath.net>
2017-10-04 10:35:49 -0400

    Creating and Testing the addLast and getLast Methods

--- src/linkedlistreview/DLinkedList.java
+++ src/linkedlistreview/DLinkedList.java
@@ -1,8 +1,27 @@
 package linkedlistreview;
 
 public class DLinkedList {
+    Node head;
     
     public DLinkedList(){
-    
+        head = null;
+    }
+    public void addLast(String value){
+        Node temp = head;
+        if(head == null){
+            head = new Node(value, null, null);
+        }else{
+            while(temp.next != null){
+                temp = temp.next;
+            }
+            temp.next = new Node(value, temp, null);
+        }
+    }
+    public String getLast(){
+        Node temp = head;
+        while(temp.next != null){
+            temp = temp.next;
+        }
+        return temp.value;
     }
 }
--- src/linkedlistreview/LinkedListReview.java
+++ src/linkedlistreview/LinkedListReview.java
@@ -5,6 +5,14 @@ public class LinkedListReview {
     public static void main(String[] args) {
         DLinkedList list = new DLinkedList();
         
+        //Kyla Testing
+        list.addLast("This");
+        System.out.println(list.getLast());
+        list.addLast("works");
+        System.out.println(list.getLast());
+        System.out.println(list);
+        
+        //Tom Testing
     }
     
 }
